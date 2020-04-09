342227755e2695e144c43218cd0ab0420b171f45
jakejoue <jakejoue@live.com>
2020-04-09 15:13:05 +0800

    mapCrs unit

--- src/ui/freecrsmap.js
+++ src/ui/freecrsmap.js
@@ -7,16 +7,17 @@ import { get, Projection } from '../extend/proj';
  */
 export default class FreeCRSMap extends Map {
     _mapCRS: any;
+    units: any;
 
     constructor(options: any) {
         let mapCRS = options.mapCRS,
             units = options.units || 'degrees';
 
         if (mapCRS) {
-            const { topTileExtent, resolutions, tileSize } = mapCRS;
+            const { topTileExtent, resolutions, tileSize, units: crsUnits2 } = mapCRS;
             const projection = new Projection({
                 code: 'mapcrs',
-                units: options.units,
+                units: crsUnits2 || options.units,
                 extent: topTileExtent || [-180, -90, 180, 90],
                 resolutions,
                 tileSize
@@ -28,6 +29,7 @@ export default class FreeCRSMap extends Map {
 
             if (projection && projection.getCode() !== 'EPSG:mapbox') {
                 mapCRS = {
+                    units: projection.getUnits(),
                     topTileExtent: projection.getExtent(),
                     resolutions: projection.getResolutions(),
                     tileSize: projection.getTileSize()
